c91231b6db6fc1409d5820daa126ef7fee16981e
nancyorgan <nancyorgan@gmail.com>
2018-03-26 10:48:48 -0700

    Cleaning up Master

--- test.R
+++ test.R
@@ -1,17 +1,1 @@
-# This is a practice run, authored by Nancy
-###This is a brand new line
-# This is an example, authored by Nancy
-##This is a test, authored by Sarah
-
-#Pretend this is a fancy function
-#with a lot of packages
-#and dplyr stuff
-#but no RC tokens
-#TESTING without Rhistory
-
-# Nancy is making a changes to Sarah’s branch!
-
-## Nancy added these changes when she created her new branch, nancy-branch1
-
-# New Monday changes
-
+# This is the master now. 
